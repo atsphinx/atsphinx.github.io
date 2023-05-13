b105f102bbae8db9d7c517d87522bb45c7d6bd4e
Kazuya Takei <myself@attakei.net>
2023-05-13 23:27:22 +0900

    refactor: Clean up conf

--- contents/conf.py
+++ contents/conf.py
@@ -1,28 +1,14 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# -- Project information
+project = "atsphinx"
+copyright = "2023, Kazuya Takei"
+author = "Kazuya Takei"
+release = "2023.5.1"
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'atsphinx'
-copyright = '2023, Kazuya Takei'
-author = 'Kazuya Takei'
-release = '2023.5.1'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# -- General configuration
 extensions = []
-
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# -- Options for HTML output
+html_theme = "alabaster"
+html_static_path = ["_static"]
