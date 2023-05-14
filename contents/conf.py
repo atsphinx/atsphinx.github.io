e45437d4da6177d7d0244c1de279804cc792cfea
Kazuya Takei <myself@attakei.net>
2023-05-14 11:30:03 +0900

    feat: Use third-party theme

--- contents/conf.py
+++ contents/conf.py
@@ -14,9 +14,15 @@ templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output
-html_theme = "alabaster"
+html_theme = "pydata_sphinx_theme"
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 html_title = project
+html_permalinks = False
+html_theme_options = {
+    "article_footer_items": [],
+    "secondary_sidebar_items": [],
+}
 
 # -- Options for extensions
 # sphinx.ext.todo
